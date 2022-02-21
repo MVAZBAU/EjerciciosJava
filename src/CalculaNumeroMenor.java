4f233d1f84f70ce2f7114d3d707f2106158df48e
MVAZBAU <mateo.vazbau@hotmail.com>
2022-02-20 21:34:25 -0600

    Nueva tarea: buscar el menor de dos números

---
+++ src/CalculaNumeroMenor.java
@@ -0,0 +1,21 @@
+/**
+ ** Nombre:             CalculaNumeroMenor
+ ** Propósito:          Buscar el número menor
+ ** Descripción:        buscar el número menor de mínimo 10 valores enteros
+ ** Retorna:
+ **
+ ** Fecha creación:     20/02/2022
+ ** Autor o creador:	MVAZBAU
+ **
+ ** Fecha modificación:
+ ** Autor modificación:
+ ** Revisión:
+ **/
+public class CalculaNumeroMenor
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+}
